118da37c64a51f4f2ab7c88e18edfbb0defdb681
Alejandro Saucedo <alejandro.saucedo@zalando.de>
2024-09-26 07:21:44 +0200

    initial
    Signed-off-by: Alejandro Saucedo <alejandro.saucedo@zalando.de>

---
+++ index.js
@@ -0,0 +1,6 @@
+
+const dt = await aq.loadCSV('data.csv'); 
+
+colnames = "time", "job", "role", "company", "industry", "org_size", "models_now", "models_future", "modality", "usecase", "components", "registry", "featurestore", "vector", "etl", "training", "platform", "serving", "infra", "time_taken", "org_setup", "lib_use", "cloud", "pct", "challenges", "satisfied", "important", "mature", "gender", "age", "country", "monitoring", "foundation"
+
+window.aq = aq
