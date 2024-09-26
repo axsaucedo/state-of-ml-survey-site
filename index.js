81cf9dba1c418253eaf780302d63e7968b37efaf
Alejandro Saucedo <alejandro.saucedo@zalando.de>
2024-09-26 17:55:04 +0200

    next iteration of charts
    Signed-off-by: Alejandro Saucedo <alejandro.saucedo@zalando.de>

--- index.js
+++ index.js
@@ -17,48 +17,119 @@ dt = dt.select(...origColNames.slice(1, -2));
 // window.colNames = colNames;
 window.dt = dt;
 
+
+// Global charts object
+var charts = [];
+
+for (let i = 0; i < origColNames.length; i++) {
+    $("#charts").prepend("<div class='col-md-3'><canvas id='chart-"+i+"'></canvas></div>")
+    const chartType = "bar" ? multiChoiceCols.includes(i) : "pie";
+    const chart = new Chart($("chart-"+i),{
+      type: chartType,
+      options: {
+        plugins: {
+            colors: {
+                forceOverride: true
+            }
+        }
+      }
+    });
+    charts.push(chart);
+}
+console.log(charts);
+
 $('#table').html(dt.toHTML()).children().attr('id', 'demo').ready(function() { loadTable() });
 
 function loadTable() {
 
-var tfConfig = {
-    base_path: 'https://unpkg.com/tablefilter@0.7.2/dist/tablefilter/',
-    alternate_rows: true,
-    rows_selected: {
-        text: 'Displayed rows: '
-    },
-    loader: true,
-    status: true,
-    status_bar: true,
+    var tfConfig = {
+        base_path: 'https://unpkg.com/tablefilter@0.7.2/dist/tablefilter/',
+        alternate_rows: true,
+        rows_selected: {
+            text: 'Displayed rows: '
+        },
+        loader: true,
+        status: true,
+        status_bar: true,
 
-	auto_filter: {
-	    delay: 500 //milliseconds
-	},
+        auto_filter: {
+            delay: 500 //milliseconds
+        },
 
-    /* sorting feature */
-    extensions: [{ name: 'sort' }]
+        /* sorting feature */
+        extensions: [{ name: 'sort' }]
 
-};
+    };
 
-// Adding column filters
-for (let i = 0; i < origColNames.length; i++) {
-	if (multiChoiceCols.includes(i)) continue;
-	tfConfig["col_" + i] = "checklist";
+    // Adding column filters
+    for (let i = 0; i < origColNames.length; i++) {
+        if (multiChoiceCols.includes(i)) continue;
+        tfConfig["col_" + i] = "checklist";
+    }
+
+
+    var tf = new TableFilter('demo', tfConfig);
+
+    tf.emitter.on([
+        'after-filtering',
+        "after-clearing-filters",
+        "initialized"
+        ], afterFilter(tf));
+
+    tf.init();
+
+    // Run once
+    //afterFilter();
+
 }
 
+function afterFilter(tf) {
+	return function () {
+		const data = tf.getFilteredData();
+        window.data = data;
+        let chartData = [];
+        for (let i = 0; i < origColNames.length; i++) {
+            chartData.push({});
+        }
+        for (let i = 0; i < data.length; i++) {
+            let row = data[i][1];
+            for (let j = 0; j < origColNames.length; j++) {
+                // Split the multi choice options
+                if (multiChoiceCols.includes(j)) {
+                    let multiValues = row[j].split(",");
+                    for (let k = 0; k < multiValues.length; k++) {
+                        if (multiValues[k] in chartData[j]) {
+                            chartData[j][multiValues[k]] += 1;
+                        } else {
+                            chartData[j][multiValues[k]] = 1;
+                        }
+                    }
+                }
+                else {
+                    if (row[j] in chartData[j]) {
+                        chartData[j][row[j]] += 1;
+                    } else {
+                        chartData[j][row[j]] = 1;
+                    }
+                }
+            }
+        }
+        window.chartData = chartData;
+        for (let chartIndex = 0; chartIndex < chartData.length; chartIndex++) {
+            updateChart(charts[chartIndex], Object.keys(chartData[chartIndex]), Object.values(chartData[chartIndex]))
+        }
+	}
 
-var tf = new TableFilter('demo', tfConfig);
-
-tf.emitter.on(['after-filtering', "after-clearing-filters"], afterFilter);
-
-tf.init();
-
-window.tf = tf;
 }
 
-function afterFilter() {
-	const data = tf.getFilteredData();
-	console.log(data);
+function updateChart(chart, labels, data) {
+    chart.data = {
+        labels: labels,
+        datasets: [{
+          data: data,
+        }]
+    }
+    chart.update()
 }
 
 window.aq = aq
